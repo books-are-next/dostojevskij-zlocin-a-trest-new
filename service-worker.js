0e3cc88bfcba6cc5698218c7c67c6513fccd7faf
jan-martinek <honza.martinek@gmail.com>
2022-06-23 10:01:25 +0000

    Deploying to docs from @ books-are-next/dostojevskij-zlocin-a-trest-new@6fdc3b544043cd513e416c8231204ec854cab13b 🚀

--- service-worker.js
+++ service-worker.js
@@ -764,7 +764,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 /// <reference lib="WebWorker" />
 require("regenerator-runtime");
 const sw = self;
-const CACHE = 'cache-ff0a17d';
+const CACHE = 'cache-6fdc3b5';
 /**
  * IndexedDB singleton wrapper used to store persistent information with an predefined {@link Schema}
  */
